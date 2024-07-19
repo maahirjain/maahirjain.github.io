b8032592e5bff32603855f6e9413e4b1555ac0f4
Maahir Jain <maahirdjain@gmail.com>
2024-07-19 22:50:24 +0530

    Implement toggle theme

--- src/common/ts/common.ts
+++ src/common/ts/common.ts
@@ -6,7 +6,7 @@ export function createMenu(): void {
         document.createElement("div")
     ];
 
-    const homeSVG: string = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z" /></svg>`;
+    const homeSVG: string = `<svg id="home" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z" /></svg>`;
 
     const aboutAnchor: HTMLElement = document.createElement("a");
     aboutAnchor.textContent = "About";
@@ -45,7 +45,7 @@ export function createMenu(): void {
 
     div1.appendChild(projectsDiv);
 
-    const moonSVG: string = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M17.75,10.95L21.25,11M18.97,15.95C19.8,15.87 20.69,17.05 20.16,17.8C19.84,18.25 19.5,18.67 19.08,19.07C15.17,23 8.84,23 4.94,19.07C1.03,15.17 1.03,8.83 4.94,4.93C5.34,4.53 5.76,4.17 6.21,3.85C6.96,3.32 8.14,4.21 8.06,5.04C7.79,7.9 8.75,10.87 10.95,13.06C13.14,15.26 16.1,16.22 18.97,15.95M17.33,17.97C14.5,17.81 11.7,16.64 9.53,14.5C7.36,12.31 6.2,9.5 6.2,9.5"/></svg>`;
+    const moonSVG: string = `<svg id="moon" class="theme-btn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M17.75,10.95L21.25,11M18.97,15.95C19.8,15.87 20.69,17.05 20.16,17.8C19.84,18.25 19.5,18.67 19.08,19.07C15.17,23 8.84,23 4.94,19.07C1.03,15.17 1.03,8.83 4.94,4.93C5.34,4.53 5.76,4.17 6.21,3.85C6.96,3.32 8.14,4.21 8.06,5.04C7.79,7.9 8.75,10.87 10.95,13.06C13.14,15.26 16.1,16.22 18.97,15.95M17.33,17.97C14.5,17.81 11.7,16.64 9.53,14.5C7.36,12.31 6.2,9.5 6.2,9.5"/></svg>`;
 
     div2.innerHTML = moonSVG;
 
@@ -81,4 +81,25 @@ export function createMenu(): void {
             projectsDropdownDiv.classList.add("hide");
         }
     });
+
+    document.getElementById("moon")!.addEventListener("click", toggleTheme);
+}
+
+export function toggleTheme(): void {
+    const moonSVG: string = `<svg id="moon" class="theme-btn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M17.75,10.95L21.25,11M18.97,15.95C19.8,15.87 20.69,17.05 20.16,17.8C19.84,18.25 19.5,18.67 19.08,19.07C15.17,23 8.84,23 4.94,19.07C1.03,15.17 1.03,8.83 4.94,4.93C5.34,4.53 5.76,4.17 6.21,3.85C6.96,3.32 8.14,4.21 8.06,5.04C7.79,7.9 8.75,10.87 10.95,13.06C13.14,15.26 16.1,16.22 18.97,15.95M17.33,17.97C14.5,17.81 11.7,16.64 9.53,14.5C7.36,12.31 6.2,9.5 6.2,9.5"/></svg>`;
+
+    const sunSVG: string = `<svg id="sun" class="theme-btn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M3.55 19.09L4.96 20.5L6.76 18.71L5.34 17.29M12 6C8.69 6 6 8.69 6 12S8.69 18 12 18 18 15.31 18 12C18 8.68 15.31 6 12 6M20 13H23V11H20M17.24 18.71L19.04 20.5L20.45 19.09L18.66 17.29M20.45 5L19.04 3.6L17.24 5.39L18.66 6.81M13 1H11V4H13M6.76 5.39L4.96 3.6L3.55 5L5.34 6.81L6.76 5.39M1 13H4V11H1M13 20H11V23H13" /></svg>`;
+    const root: HTMLElement = document.documentElement;
+
+    if (root.classList.contains("dark-theme")) {
+        document.querySelector(".menu > div:last-child > svg")!.outerHTML =
+            sunSVG;
+        document.getElementById("sun")!.addEventListener("click", toggleTheme);
+        root.className = "light-theme";
+    } else if (root.classList.contains("light-theme")) {
+        document.querySelector(".menu > div:last-child > svg")!.outerHTML =
+            moonSVG;
+        document.getElementById("moon")!.addEventListener("click", toggleTheme);
+        root.className = "dark-theme";
+    }
 }
