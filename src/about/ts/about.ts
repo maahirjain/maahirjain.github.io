c79ef77470def9a1029f76eaf43b71fb78043c76
Maahir Jain <maahirdjain@gmail.com>
2024-07-30 06:29:43 +0530

    Add event listeners to about page

--- src/about/ts/about.ts
+++ src/about/ts/about.ts
@@ -9,6 +9,10 @@ import pawnPRORES from "../assets/pawn_PRORES.mov";
 createMenu();
 loadTheme();
 
+const helloOption = document.getElementById("hello-option");
+const animationBtn = document.getElementById("animation-btn");
+const chessBtn = document.getElementById("chess-btn");
+
 const donutVideoElement = document.createElement("video");
 
 const donutSourceMP4 = document.createElement("source");
@@ -22,13 +26,13 @@ donutSourceWEBM.type = "video/webm";
 donutVideoElement.appendChild(donutSourceMP4);
 donutVideoElement.appendChild(donutSourceWEBM);
 
-document.getElementById("animation-btn")?.appendChild(donutVideoElement);
+animationBtn?.appendChild(donutVideoElement);
 
 const pawnVideoElement = document.createElement("video");
-pawnVideoElement.autoplay = true;
-pawnVideoElement.loop = true;
 
 const pawnSourceMP4 = document.createElement("source");
+pawnSourceMP4.src = pawnPRORES;
+pawnSourceMP4.type = "video/mp4; codecs=hvc1";
 
 const pawnSourceWEBM = document.createElement("source");
 pawnSourceWEBM.src = pawnVP9;
@@ -36,5 +40,94 @@ pawnSourceWEBM.type = "video/webm";
 
 pawnVideoElement.appendChild(pawnSourceMP4);
 pawnVideoElement.appendChild(pawnSourceWEBM);
+chessBtn?.appendChild(pawnVideoElement);
 
-document.getElementById("chess-btn")?.appendChild(pawnVideoElement);
+animationBtn?.addEventListener("mouseover", () => {
+    donutVideoElement.loop = true;
+    donutVideoElement.play();
+});
+
+animationBtn?.addEventListener("mouseout", () => {
+    donutVideoElement.loop = false;
+    donutVideoElement.pause();
+});
+
+chessBtn?.addEventListener("mouseover", () => {
+    pawnVideoElement.loop = true;
+    pawnVideoElement.play();
+});
+
+chessBtn?.addEventListener("mouseout", () => {
+    pawnVideoElement.loop = false;
+    pawnVideoElement.pause();
+});
+
+function select(option: string) {
+    if (option === "animation") {
+        animationBtn?.classList.add("selected");
+        chessBtn?.classList.remove("selected");
+        helloOption?.classList.remove("selected");
+
+        donutVideoElement.loop = true;
+        donutVideoElement.play();
+        pawnVideoElement.loop = false;
+        pawnVideoElement.pause();
+        helloOption!.style.animation = "";
+    } else if (option === "chess") {
+        chessBtn?.classList.add("selected");
+        animationBtn?.classList.remove("selected");
+        helloOption?.classList.remove("selected");
+
+        pawnVideoElement.loop = true;
+        pawnVideoElement.play();
+        donutVideoElement.loop = false;
+        donutVideoElement.pause();
+        helloOption!.style.animation = "";
+    } else {
+        helloOption?.classList.add("selected");
+        chessBtn?.classList.remove("selected");
+        animationBtn?.classList.remove("selected");
+
+        pawnVideoElement.loop = false;
+        pawnVideoElement.pause();
+        donutVideoElement.loop = false;
+        donutVideoElement.pause();
+        helloOption!.style.animation = "wave 2.1s 0.6s infinite";
+    }
+}
+
+animationBtn?.addEventListener("click", () => {
+    select("animation");
+});
+
+chessBtn?.addEventListener("click", () => {
+    select("chess");
+});
+
+helloOption?.addEventListener("click", () => {
+    select("hello");
+});
+
+document.addEventListener("keydown", (e) => {
+    const key = e.key;
+
+    if (helloOption?.classList.contains("selected")) {
+        if (key === "ArrowDown" || key === "ArrowRight") {
+            select("animation");
+        } else if (key === "ArrowUp" || key === "ArrowLeft") {
+            select("chess");
+        }
+    } else if (animationBtn?.classList.contains("selected")) {
+        if (key === "ArrowDown" || key === "ArrowRight") {
+            select("chess");
+        } else if (key === "ArrowUp" || key === "ArrowLeft") {
+            select("hello");
+        }
+    } else if (chessBtn?.classList.contains("selected")) {
+        if (key === "ArrowDown" || key === "ArrowRight") {
+            select("hello");
+        } else if (key === "ArrowUp" || key === "ArrowLeft") {
+            select("animation");
+        }
+    }
+});
