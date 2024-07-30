a343d5fe7f0f9ded92e1bccd536fa9ced6af6dc1
Maahir Jain <maahirdjain@gmail.com>
2024-07-31 04:09:29 +0530

    Implement carousel animation

---
+++ src/projects/common/ts/projects.ts
@@ -0,0 +1,107 @@
+const smallDivArr: HTMLElement[] = Array.from(
+    document.querySelectorAll(".small")
+);
+
+function checkDivWidth(div: HTMLElement) {
+    const divWidth = div.offsetWidth;
+
+    if (divWidth <= 438) {
+        div.classList.remove("small");
+        div.classList.add("x-small");
+    } else {
+        div.classList.remove("x-small");
+        div.classList.add("small");
+    }
+}
+
+smallDivArr.forEach((div) => {
+    const observer = new MutationObserver(() => {
+        checkDivWidth(div);
+    });
+    observer.observe(div, { attributes: true, childList: true, subtree: true });
+
+    checkDivWidth(div);
+
+    window.addEventListener("resize", () => {
+        checkDivWidth(div);
+    });
+    window.addEventListener("load", () => {
+        checkDivWidth(div);
+    });
+});
+
+const divArr: HTMLElement[] = Array.from(
+    document.querySelector(".grid")!.querySelectorAll(":scope > div")
+);
+
+const currDivArr: number[] = [];
+let index = -1;
+
+divArr.forEach((div) => {
+    if (div.querySelector(".carousel") != null) {
+        currDivArr.push(2);
+        index++;
+
+        const nextBtn = div.querySelector(".next");
+        const prevBtn = div.querySelector(".prev");
+
+        nextBtn?.addEventListener("click", () => {
+            rotate("next", div, index);
+        });
+
+        prevBtn?.addEventListener("click", () => {
+            rotate("prev", div, index);
+        });
+    }
+});
+
+function rotate(btn: string, div: HTMLElement, index: number) {
+    const currDiv = currDivArr[index];
+
+    const carouselDivArr: HTMLElement[] = Array.from(
+        div.querySelector(".carousel")!.querySelectorAll(":scope > div")
+    );
+    const numOfImgs = carouselDivArr.length;
+
+    const carousel: HTMLElement | null = div.querySelector(".carousel");
+
+    function newRotation(deg: number) {
+        if (btn === "next") {
+            return deg - 60;
+        } else {
+            return deg + 60;
+        }
+    }
+
+    function animate() {
+        const currRotation = 120 - 60 * currDiv;
+        carousel?.animate(
+            [
+                { transform: `rotateY(${currRotation}deg)` },
+                { transform: `rotateY(${newRotation(currRotation)}deg)` }
+            ],
+            {
+                duration: 1000,
+                iterations: 1
+            }
+        );
+
+        carousel!.style.transform = `rotateY(${newRotation(currRotation)}deg)`;
+    }
+
+    if (btn === "next") {
+        if (currDiv === numOfImgs) {
+            rotate("prev", div, index);
+        } else {
+            animate();
+            currDivArr[index] = currDiv + 1;
+        }
+    } else if (btn == "prev") {
+        if (currDiv === 1) {
+            rotate("next", div, index);
+        } else {
+            animate();
+            currDivArr[index] = currDiv - 1;
+        }
+    }
+}
--- src/projects/featured/ts/featured.ts
+++ src/projects/featured/ts/featured.ts
@@ -2,6 +2,7 @@ import "../../../common/styles/common.scss";
 import { createMenu, loadTheme } from "../../../common/ts/common";
 import "../../common/styles/projects.scss";
 import "../styles/featured.scss";
+import "../../common/ts/projects";
 
 createMenu();
 loadTheme();
