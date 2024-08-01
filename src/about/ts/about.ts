97769791705f836482583308dc8397ed2b348d60
Maahir Jain <maahirdjain@gmail.com>
2024-08-01 17:41:34 +0530

    Create chess about layout

--- src/about/ts/about.ts
+++ src/about/ts/about.ts
@@ -86,6 +86,8 @@ function select(option: string) {
         chessBtn?.classList.remove("selected");
         helloOption?.classList.remove("selected");
 
+        load("animation");
+
         donutVideoElement.loop = true;
         donutVideoElement.play();
         pawnVideoElement.loop = false;
@@ -157,37 +159,37 @@ function load(option: string) {
 
     if (option === "hello") {
         contentDiv!.innerHTML = `<h2>Hi, I'm Maahir Jain</h2>
-        <p>I'm a junior at <a href="https://www.cc.gatech.edu/" target="_blank" rel="noopener noreferrer" class="gt">Georgia Tech</a>, majoring in computer science. My work is motivated by my commitment to learning, creating, and sharing.</p>
+            <p>I'm a junior at <a href="https://www.cc.gatech.edu/" target="_blank" rel="noopener noreferrer" class="gt">Georgia Tech</a>, majoring in computer science. My work is motivated by my commitment to learning, creating, and sharing.</p>
 
-        <div class="slider-container">
-            <div class="learning active">
-                <h2>LEARNING</h2>
-                <div class="content">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, dolore. Iusto accusantium officia itaque reiciendis iure recusandae, impedit optio, incidunt sit obcaecati labore ipsum ullam dolorem distinctio? Nesciunt quaerat vero repellat ratione nobis, sed debitis eaque sapiente. Sequi at, eos deserunt eaque quis beatae ut veritatis explicabo, nobis quo, est reprehenderit adipisci. Veritatis nobis dolorem quaerat ab dignissimos sit corporis sunt explicabo. Quidem numquam porro eius, provident culpa fugit error nesciunt dicta assumenda omnis facere aspernatur voluptatem nostrum, quam corrupti adipisci quia tempore id molestiae repudiandae quae esse iste quo asperiores. Dicta quam quia quae possimus quod quisquam incidunt ipsam.
+            <div class="slider-container">
+                <div class="learning active">
+                    <h2>LEARNING</h2>
+                    <div class="content">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, dolore. Iusto accusantium officia itaque reiciendis iure recusandae, impedit optio, incidunt sit obcaecati labore ipsum ullam dolorem distinctio? Nesciunt quaerat vero repellat ratione nobis, sed debitis eaque sapiente. Sequi at, eos deserunt eaque quis beatae ut veritatis explicabo, nobis quo, est reprehenderit adipisci. Veritatis nobis dolorem quaerat ab dignissimos sit corporis sunt explicabo. Quidem numquam porro eius, provident culpa fugit error nesciunt dicta assumenda omnis facere aspernatur voluptatem nostrum, quam corrupti adipisci quia tempore id molestiae repudiandae quae esse iste quo asperiores. Dicta quam quia quae possimus quod quisquam incidunt ipsam.
+                    </div>
+                </div>
+
+                <div class="creating inactive">
+                    <h2>CREATING</h2>
+                    <div class="content">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet sed quis, dolorum consequatur animi tempore iusto eligendi quia modi error nostrum deserunt ipsa suscipit! Alias aliquid maxime iusto labore temporibus eos, a fuga natus officiis tenetur consectetur consequatur assumenda at ipsum corrupti officia numquam? Rerum excepturi dolorum eveniet doloribus fugiat quae dolore dolorem pariatur nam est, minima commodi optio maxime in magni harum odio delectus iure. Nobis nam praesentium sint amet at vel adipisci placeat quae labore culpa, exercitationem ullam. Obcaecati quisquam labore ipsum accusantium aperiam, odio dolore? Qui minus deleniti officiis tempora. Aperiam odit porro nisi iure atque culpa?
+                    </div>
+                </div>
+
+                <div class="sharing inactive">
+                    <h2>SHARING</h2>
+                    <div class="content">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed modi debitis sequi hic laudantium, delectus perspiciatis sunt rem doloribus voluptatibus tempore eveniet officiis incidunt excepturi voluptatum qui, ratione, expedita minima! Nobis dolor quia ducimus hic ipsum necessitatibus quae perferendis cupiditate praesentium sequi, consequuntur nostrum voluptates error minima vero accusamus itaque, aut iure possimus. Ut quia voluptas exercitationem nemo tenetur, eos fugiat inventore. Doloremque adipisci nobis saepe sed ut fugiat tenetur velit error id culpa eos consequuntur quisquam, quam, facilis minima dolor amet non! Optio unde nihil veniam suscipit ducimus, sit est aliquam culpa ut deleniti facilis quis esse asperiores natus.
+                    </div>
                 </div>
             </div>
 
-            <div class="creating inactive">
-                <h2>CREATING</h2>
-                <div class="content">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet sed quis, dolorum consequatur animi tempore iusto eligendi quia modi error nostrum deserunt ipsa suscipit! Alias aliquid maxime iusto labore temporibus eos, a fuga natus officiis tenetur consectetur consequatur assumenda at ipsum corrupti officia numquam? Rerum excepturi dolorum eveniet doloribus fugiat quae dolore dolorem pariatur nam est, minima commodi optio maxime in magni harum odio delectus iure. Nobis nam praesentium sint amet at vel adipisci placeat quae labore culpa, exercitationem ullam. Obcaecati quisquam labore ipsum accusantium aperiam, odio dolore? Qui minus deleniti officiis tempora. Aperiam odit porro nisi iure atque culpa?
-                </div>
-            </div>
-
-            <div class="sharing inactive">
-                <h2>SHARING</h2>
-                <div class="content">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed modi debitis sequi hic laudantium, delectus perspiciatis sunt rem doloribus voluptatibus tempore eveniet officiis incidunt excepturi voluptatum qui, ratione, expedita minima! Nobis dolor quia ducimus hic ipsum necessitatibus quae perferendis cupiditate praesentium sequi, consequuntur nostrum voluptates error minima vero accusamus itaque, aut iure possimus. Ut quia voluptas exercitationem nemo tenetur, eos fugiat inventore. Doloremque adipisci nobis saepe sed ut fugiat tenetur velit error id culpa eos consequuntur quisquam, quam, facilis minima dolor amet non! Optio unde nihil veniam suscipit ducimus, sit est aliquam culpa ut deleniti facilis quis esse asperiores natus.
-                </div>
-            </div>
-        </div>
-
-        <div class="media">
-            <div class="speech">jainmaahir1@gmail.com</div>
-            <svg id="github-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z" /></svg>
-            <svg id="gmail-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20,18H18V9.25L12,13L6,9.25V18H4V6H5.2L12,10.25L18.8,6H20M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z" /></svg>
-            <svg id="linkedin-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19 3A2 2 0 0 1 21 5V19A2 2 0 0 1 19 21H5A2 2 0 0 1 3 19V5A2 2 0 0 1 5 3H19M18.5 18.5V13.2A3.26 3.26 0 0 0 15.24 9.94C14.39 9.94 13.4 10.46 12.92 11.24V10.13H10.13V18.5H12.92V13.57C12.92 12.8 13.54 12.17 14.31 12.17A1.4 1.4 0 0 1 15.71 13.57V18.5H18.5M6.88 8.56A1.68 1.68 0 0 0 8.56 6.88C8.56 5.95 7.81 5.19 6.88 5.19A1.69 1.69 0 0 0 5.19 6.88C5.19 7.81 5.95 8.56 6.88 8.56M8.27 18.5V10.13H5.5V18.5H8.27Z" /></svg>
-        </div>`;
+            <div class="media">
+                <div class="speech">jainmaahir1@gmail.com</div>
+                <svg id="github-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z" /></svg>
+                <svg id="gmail-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20,18H18V9.25L12,13L6,9.25V18H4V6H5.2L12,10.25L18.8,6H20M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z" /></svg>
+                <svg id="linkedin-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19 3A2 2 0 0 1 21 5V19A2 2 0 0 1 19 21H5A2 2 0 0 1 3 19V5A2 2 0 0 1 5 3H19M18.5 18.5V13.2A3.26 3.26 0 0 0 15.24 9.94C14.39 9.94 13.4 10.46 12.92 11.24V10.13H10.13V18.5H12.92V13.57C12.92 12.8 13.54 12.17 14.31 12.17A1.4 1.4 0 0 1 15.71 13.57V18.5H18.5M6.88 8.56A1.68 1.68 0 0 0 8.56 6.88C8.56 5.95 7.81 5.19 6.88 5.19A1.69 1.69 0 0 0 5.19 6.88C5.19 7.81 5.95 8.56 6.88 8.56M8.27 18.5V10.13H5.5V18.5H8.27Z" /></svg>
+            </div>`;
 
         const learningDiv: HTMLElement | null =
             document.querySelector(".learning");
@@ -256,14 +258,32 @@ function load(option: string) {
             ?.addEventListener("click", () => {
                 window.open("https://linkedin.com/in/maahirjain/", "_blank");
             });
+    } else if (option === "animation") {
+        contentDiv!.innerHTML = `<h2>Animation</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia repudiandae veniam dolores beatae corporis nesciunt officiis quis temporibus accusamus iusto, aut, libero atque tempora possimus a repellat? Vitae beatae natus delectus, sed dicta iure blanditiis perspiciatis placeat culpa sapiente officiis, doloribus enim pariatur quo dolorum modi iusto quas! Voluptatum, quos?
+            </p>
+
+            <div class="animation-img-container">
+                <div>
+                    <video controls></video>
+                    <p>A 3D animation of a donut scene, created following <a href="https://www.youtube.com/playlist?list=PLjEaoINr3zgEPv5y--4MKpciLaoQYZB1Z" target="_blank" rel="noopener noreferrer">Blender Guru's Beginner Donut Tutorial</a></p>
+                </div>
+
+                <div>
+                    <img alt="Blender vending machine render">
+                    <p>A 3D stylistic model of a vending machine, created following <a href="https://youtu.be/BzTN2yTgTj0?si=Vx3zD4dh1Xbb3-f8" target="_blank" rel="noopener noreferrer">lacruzo's Grease Pencil Tutorial</a></p>
+                </div>
+            </div>`;
+
+        const animationImgContainer = document.querySelector(
+            ".animation-img-container"
+        );
+
+        animationImgContainer!.querySelector("video")!.src = donutBlender;
+        animationImgContainer!.querySelector("img")!.src =
+            vendingMachineBlender;
     }
 }
 
 // load("hello");
-
-const animationImgContainer = document.querySelector(
-    ".animation-img-container"
-);
-
-animationImgContainer!.querySelector("video")!.src = donutBlender;
-animationImgContainer!.querySelector("img")!.src = vendingMachineBlender;
