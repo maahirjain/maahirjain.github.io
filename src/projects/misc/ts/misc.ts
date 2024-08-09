b10d1ebbdb723dc1943e0467b7384df33773d37a
Maahir Jain <maahirdjain@gmail.com>
2024-08-09 12:56:26 -0400

    Add LaTeX template live preview

--- src/projects/misc/ts/misc.ts
+++ src/projects/misc/ts/misc.ts
@@ -3,6 +3,10 @@ import { createMenu, loadTheme } from "../../../common/ts/common";
 import "../../common/styles/projects.scss";
 import "../styles/misc.scss";
 import "../../common/ts/projects";
+import pdfFile from "../assets/pdfs/Cool_LaTeX_Template.pdf";
 
 createMenu();
 loadTheme();
+
+(<HTMLAnchorElement>document.getElementById("latex-template-preview")).href =
+    pdfFile;
